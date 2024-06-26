89089094186a38a3625f409d185fa53442f41c5d
Igor <qwerty2244668800@gmail.com>
2024-06-26 15:43:36 +0300

    category, transaction and user resources are added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionModule } from './transaction/transaction.module';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
+  imports: [TransactionModule, UserModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })

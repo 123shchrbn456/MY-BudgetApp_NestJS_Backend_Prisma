20870429b4f6f6ba2be9bccea360b5502ba0c8d0
Igor <qwerty2244668800@gmail.com>
2024-07-02 15:37:40 +0300

    Authorization and Registration are implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionModule } from './transaction/transaction.module';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TransactionModule, UserModule, CategoryModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot(), TransactionModule, UserModule, CategoryModule, AuthModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Prisma } from '@prisma/client';
+import { hash } from 'argon2';
+import { AuthDto } from '../auth/dto/auth.dto';
 
 @Injectable()
 export class UserService {
     constructor(private prisma: PrismaService) {}
 
-    create(dto: Prisma.UserCreateInput) {
+    async create(dto: AuthDto) {
+        const user = {
+            email: dto.email,
+            // name: '',
+            password: await hash(dto.password),
+        };
+
         return this.prisma.user.create({
-            data: dto,
+            data: user,
         });
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} user`;
+    getById(id: number) {
+        return this.prisma.user.findUnique({
+            where: {
+                id,
+            },
+        });
+    }
+
+    getByEmail(email: string) {
+        return this.prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
     }
 }
